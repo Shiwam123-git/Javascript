d497ef251a478d6750c0a41adce6707f9aae9a9b
Shiwam123-git <Shiwamankit9@gmail.com>
2024-12-27 15:14:15 +0000

    Update

--- .devcontainer/shiwam/new.js
+++ .devcontainer/shiwam/new.js
@@ -1,6 +1,16 @@
 let arr=[5,6,7,8]
-
+res=[]
+idx=0
 arr.forEach((data)=>{
-    console.log(data);
+    
+    res=[data*10]
+    console.log(res,idx++);
+    
+    
+    
+    
+
     
 })
+console.log(arr);
+
