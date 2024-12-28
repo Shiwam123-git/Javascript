d2cb74320c537f85436c84938b4b6e8ca93f3e1a
Shiwam123-git <Shiwamankit9@gmail.com>
2024-12-28 16:08:56 +0000

    new

--- .devcontainer/shiwam/promises.js
+++ .devcontainer/shiwam/promises.js
@@ -2,6 +2,11 @@
 let promise=new Promise(function(resolved , reject){
     setTimeout(()=>{
         console.log('heloo shiwam');
+        resolved()
         
     },2000)
+}).then(()=>{
+    console.log("promise is completed");
+    
 })
+
