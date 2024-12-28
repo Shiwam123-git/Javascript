3013733009b2084a01084fd1358f72f82db12bff
Shiwam123-git <Shiwamankit9@gmail.com>
2024-12-28 10:16:53 +0000

    Done

--- .devcontainer/shiwam/revString.js
+++ .devcontainer/shiwam/revString.js
@@ -1,9 +1,44 @@
+//Direct funtion invoke with return Method 1
 function str(name){
 
     for(i in name){
+       return name.split('').reverse().toString();  
+    }   
+}
 
-        return name.split('').reverse().toString()
-    }
+console.log(str('shiwam'));
+
+//Direct funtion without invoke with return Method 2
+
+function str1(name){
+
+    for(i in name){
+        
+    }   
+    console.log("orignal string=",name);
+    
+    Split=name.split('')//converted to Array
+    rev=Split.reverse()//reversed in array 
+    string=rev.toString()//back to string 
+    console.log("Reverse string=",string);
+    
 
 }
-str('shiwam')
+
+str1('shiwam');
+
+
+//Reverse string using without in built function 
+function str2(name) {
+    for (let index = name.length-1; index>=0; index--) {
+            
+        console.log(name[index]);
+        
+    }
+    
+    
+    
+}
+str2('hello');
+
+ 
