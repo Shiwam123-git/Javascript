a5b543f1772f99df42ae8a07ca1a9dafbf184ca9
Shiwam123-git <Shiwamankit9@gmail.com>
2024-12-30 11:46:06 +0000

    new change

--- .devcontainer/shiwam/promises.js
+++ .devcontainer/shiwam/promises.js
@@ -10,3 +10,27 @@ let promise=new Promise(function(resolved , reject){
     
 })
 
+let promises2= new Promise(function(Resolved , Reject){
+
+    setTimeout((a,b) => {
+        if(a>b){
+             console.log("Promise 2 is in progress");
+             resolved()
+             
+        }else {
+            console.log("Promise2 reject");
+            reject()
+            
+        }
+        
+    }, 2000);
+}).then((user)=>{
+    user(4,5)
+    console.log("passed completd 2");
+    
+
+}).catch((nouser)=>{
+     console.log("promise failed ");
+     
+})
+
